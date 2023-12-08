1e0f46a1069fd50f54fccfc951bde8c19810448f
dtdao <kwalitycontrol@gmail.com>
2023-12-08 18:24:04 +0900

    main logic into goroutine, add rate flag

--- cmd/check.go
+++ cmd/check.go
@@ -15,8 +15,11 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var rate int
+
 func init() {
 	rootCmd.AddCommand(checkVpnCmd)
+	checkVpnCmd.Flags().IntVarP(&rate, "rate", "r", 1, "Specify the number of minutes interval you wish to poll")
 }
 
 var checkVpnCmd = &cobra.Command{
@@ -27,10 +30,11 @@ var checkVpnCmd = &cobra.Command{
 }
 
 const (
-	ipify       = "https://api.ipify.org?format=json"
-	timeout     = 10 * time.Second
-	pollingRate = 1 * time.Second
+	ipify   = "https://api.ipify.org?format=json"
+	timeout = 10 * time.Second
 )
+
+var pollingRate = 1 * time.Minute
 
 func checkVpn(cmd *cobra.Command, args []string) {
 	interrupt := make(chan os.Signal, 1)
@@ -48,65 +52,74 @@ func checkVpn(cmd *cobra.Command, args []string) {
 		ip = validIp.String()
 	}
 
-	// Reuse the HTTP client
-	response, err := client.Get(ipify)
-	if err != nil {
-		fmt.Println("Error initial IP check:", err)
-		return
-	}
-	defer response.Body.Close()
+	go func() {
 
-	body, err := io.ReadAll(response.Body)
-	if err != nil {
-		fmt.Println("Error reading initial response body:", err)
-		return
-	}
-
-	var initialPublicIp PublicIp
-	err = json.Unmarshal(body, &initialPublicIp)
-	if err != nil {
-		fmt.Println("Error unmarshalling initial JSON:", err)
-		return
-	}
-
-	fmt.Println(fmt.Sprintf("Watching for changes in the IP: %s", ip))
-
-	for {
-		select {
-		case <-interrupt:
-			fmt.Println("Interrupt signal received. Stopping...")
+		// Reuse the HTTP client
+		response, err := client.Get(ipify)
+		if err != nil {
+			fmt.Println("Error initial IP check:", err)
 			return
-		default:
-			response, err := client.Get(ipify)
-			if err != nil {
-				handleHTTPError(err)
-				time.Sleep(pollingRate)
-				continue
-			}
-			defer response.Body.Close()
-
-			body, err := io.ReadAll(response.Body)
-			if err != nil {
-				fmt.Println("Error reading response body:", err)
-				time.Sleep(pollingRate)
-				continue
-			}
-
-			var publicIp PublicIp
-			err = json.Unmarshal(body, &publicIp)
-			if err != nil {
-				fmt.Println("Error unmarshalling JSON:", err)
-				time.Sleep(pollingRate)
-				continue
-			}
-
-			if publicIp.IP != ip {
-				handleIPChange(ip, client)
-			}
-
-			time.Sleep(pollingRate)
 		}
-	}
+		defer response.Body.Close()
+
+		body, err := io.ReadAll(response.Body)
+		if err != nil {
+			fmt.Println("Error reading initial response body:", err)
+			return
+		}
+
+		var initialPublicIp PublicIp
+		err = json.Unmarshal(body, &initialPublicIp)
+		if err != nil {
+			fmt.Println("Error unmarshalling initial JSON:", err)
+			return
+		}
+
+		fmt.Println(fmt.Sprintf("Watching for changes in the IP: %s", ip))
+
+		for {
+			select {
+			case <-interrupt:
+				fmt.Println("\nInterrupt signal received. Stopping...")
+				return
+			default:
+				response, err := client.Get(ipify)
+				if rate != 1 {
+					pollingRate = time.Minute * time.Duration(rate)
+				}
+				if err != nil {
+					handleHTTPError(err)
+					time.Sleep(pollingRate)
+					continue
+				}
+				defer response.Body.Close()
+
+				body, err := io.ReadAll(response.Body)
+				if err != nil {
+					fmt.Println("Error reading response body:", err)
+					time.Sleep(pollingRate)
+					continue
+				}
+
+				var publicIp PublicIp
+				err = json.Unmarshal(body, &publicIp)
+				if err != nil {
+					fmt.Println("Error unmarshalling JSON:", err)
+					time.Sleep(pollingRate)
+					continue
+				}
+
+				if publicIp.IP != ip {
+					handleIPChange(ip, client)
+				}
+
+				time.Sleep(pollingRate)
+			}
+		}
+	}()
+
+	<-interrupt
+	fmt.Println("\nExiting...")
 }
 
 func handleHTTPError(err error) {
--- cmd/root.go
+++ cmd/root.go
@@ -14,9 +14,15 @@ type PublicIp struct {
 var rootCmd = &cobra.Command{
 	Use:   "Vpn Checker",
 	Short: "Periodically checks if you are still behind a vpn",
+	Long: `Vpn Checker will poll a ip api back that will return your current global ip
+It will just compared your current global ip with your vpn global ip address. 
+If the ip address do not match then you will get a notification that you are currently
+not connected your VPN service.`,
 }
 
 func Execute() {
+	rootCmd.CompletionOptions.DisableDefaultCmd = true
+
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
